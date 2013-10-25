387c82dc41dc2f049878d546775daed674b086d6
Michael Zipp <mzipp3@mail.gatech.edu>
2013-10-25 00:49:33 -0400

    working on network
    Look! a blank open field!

---
+++ Assets/Scripts/ServerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI(){
+		if(GUILayout.Button("Start Server")){
+			//Use NAT punchthrough if no public IP present
+			var useNat = !Network.HavePublicAddress();
+			Network.InitializeServer(32, 25002, useNat);
+			MasterServer.RegisterHost("RealityFlux0.1", "Hosted Game", "comment goes here");
+		}
+	}
+}
