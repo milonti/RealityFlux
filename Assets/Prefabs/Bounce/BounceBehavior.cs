ef8fe92d6f9d1c0bc0c875fcc5c4dc552a98bf4f
Maxwell McRae <maxwellmcrae@gmail.com>
2013-11-25 16:00:00 -0500

    ignored wall and changed bouncer

--- Assets/Prefabs/Bounce/BounceBehavior.cs
+++ Assets/Prefabs/Bounce/BounceBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class BounceBehavior : MonoBehaviour {
 	
-	public float speed = 600;
+	public float speed = 200;
 	
 	public CharacterController bounceC;
 	double counter=0;
