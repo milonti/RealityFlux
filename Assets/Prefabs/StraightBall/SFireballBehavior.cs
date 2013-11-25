1a5c474f422194411b78e7cba02d284654668314
Michael Zipp <mzipp3@mail.gatech.edu>
2013-11-25 14:51:17 -0500

    Revert "fff"
    This reverts commit f17ff18da470125227243b01788a7ea376f1b8e3.

--- Assets/Prefabs/StraightBall/SFireballBehavior.cs
+++ Assets/Prefabs/StraightBall/SFireballBehavior.cs
@@ -15,9 +15,6 @@ public class SFireballBehavior : MonoBehaviour {
 	
 	public AudioClip body_hit_sound;
 	
-	public bool dest = false;
-	public float dTime = 0;
-	
 	// Use this for initialization
 	void Start () {	
 	}
@@ -26,23 +23,16 @@ public class SFireballBehavior : MonoBehaviour {
 	void Update () {
 		//life+=Time.deltaTime;
 		
-		if (!dest) {
-			fireC.Move(transform.forward * speed * Time.deltaTime);
-			//Vector3 direction=(enemy.transform.position-fireC.transform.localPosition);
-			//direction.Normalize();
-			//Quaternion test=new Quaternion(0,0,0,0);
-			//test.SetLookRotation(direction,fireC.transform.up);
-			//transform.rotation = Quaternion.Lerp(from.rotation, to.rotation, Time.time * speed);
-			//fireC.transform.rotation = Quaternion.Lerp(fireC.transform.rotation,test, Time.deltaTime);
-			shittyCollisionDetection(5);
-			life+=Time.deltaTime;
-			if(life >=3) {
-				dest = true;
-				renderer.enabled = false;
-			}
-		}
-		if(dest) dTime += Time.deltaTime;
-		if(dTime >= 5) Destroy(gameObject); 
+		fireC.Move(transform.forward * speed * Time.deltaTime);
+		//Vector3 direction=(enemy.transform.position-fireC.transform.localPosition);
+		//direction.Normalize();
+		//Quaternion test=new Quaternion(0,0,0,0);
+		//test.SetLookRotation(direction,fireC.transform.up);
+		//transform.rotation = Quaternion.Lerp(from.rotation, to.rotation, Time.time * speed);
+		//fireC.transform.rotation = Quaternion.Lerp(fireC.transform.rotation,test, Time.deltaTime);
+		shittyCollisionDetection(5);
+		life+=Time.deltaTime;
+		if(life >=3) Destroy(gameObject);
 	}
 	public void setEnemy(GameObject e){
 		enemy=e;	
@@ -53,22 +43,20 @@ public class SFireballBehavior : MonoBehaviour {
 	
 	void OnControllerColliderHit(ControllerColliderHit hit){
 		//WizardGUIScript.addHealth(-5);
-		//GameObject explode = (GameObject)Instantiate(explosionParticles, transform.position, new Quaternion(0f, 0f, 0f, 0f));
+		GameObject explode = (GameObject)Instantiate(explosionParticles, transform.position, new Quaternion(0f, 0f, 0f, 0f));
 		if(hit.collider.name.Equals("Hitbox")){
 				//insert hit something report here.
 				//Destroy(gameObject);
 			}
-		
-		dest = true;
+		Destroy(gameObject);
 	}
-	
 	public void shittyCollisionDetection(int lose){
 		if(Vector3.Distance(fireC.transform.position,control.transform.position)<10){
 			Debug.Log("hit something");
 			control.audio.PlayOneShot(body_hit_sound);
 			control.GetComponent<CarpetControl>().detract(lose);
-			dest = true;
-			renderer.enabled = false;
+			Destroy(gameObject);
+			
 		}
 		
 	}
